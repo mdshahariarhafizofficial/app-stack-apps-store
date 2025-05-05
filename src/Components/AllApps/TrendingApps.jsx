169de2f8e71bce1c7f7327e7094db4130df6cd36
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-05-05 21:25:43 +0600

    Trending section slider added

--- src/Components/AllApps/TrendingApps.jsx
+++ src/Components/AllApps/TrendingApps.jsx
@@ -1,13 +1,69 @@
 import React from 'react';
 import AppsCard from '../AppsCard/AppsCard';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const TrendingApps = ({trending}) => {
-    
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 1000,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        initialSlide: 0,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              initialSlide: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      };
+
+
     
     return (
         <div className='my-10 px-5 lg:px-0'>
             <h2 className='text-3xl font-bold mb-5'>Trending Apps</h2>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6'>
+
+            {/* Slider */}
+            <div className="slider-container">
+                <Slider {...settings}>
+
+                        {
+                            trending
+                            .sort( (a,b) => a.rating < b.rating ? 1 : -1 )
+                            .map(app => <AppsCard
+                                key={app.id} 
+                                app={app}
+                                ></AppsCard>)
+                        }
+                </Slider>
+            </div>
+
+            {/* <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6'>
                 {
                     trending
                     .sort( (a,b) => a.rating < b.rating ? 1 : -1 )
@@ -16,7 +72,7 @@ const TrendingApps = ({trending}) => {
                         app={app}
                         ></AppsCard>)
                 }
-            </div>
+            </div> */}
         </div>
     );
 };
