e7b2999a14b94c2abfd5efa17a1a12d7ee14c48c
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-05-05 14:12:43 +0600

    Trending app card design done

--- src/Components/AllApps/TrendingApps.jsx
+++ src/Components/AllApps/TrendingApps.jsx
@@ -6,8 +6,8 @@ const TrendingApps = ({trending}) => {
     
     return (
         <div className='my-10 px-5 lg:px-0'>
-            <h2 className='text-2xl font-bold'>Trending Apps</h2>
-            <div>
+            <h2 className='text-3xl font-bold mb-5'>Trending Apps</h2>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6'>
                 {
                     trending.map(app => <TrendingAppsCard
                         key={app.id} 
--- src/Components/AppsCard/TrendingAppsCard.jsx
+++ src/Components/AppsCard/TrendingAppsCard.jsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React from "react";
+import { FaDownload, FaStar } from "react-icons/fa";
 
-const TrendingAppsCard = ({app}) => {
-    const {name, thumbnail, downloads, rating} = app;
-    return (
-        <div>
-            <img src={thumbnail} alt="" />
-            <h2>{name}</h2>
-            <p>{rating}</p>
-            <p>{downloads}</p>
+const TrendingAppsCard = ({ app }) => {
+  const { name, thumbnail, downloads, rating } = app;
+  return (
+    <div>
+      <div className="card bg-base-100 shadow-sm border-1 border-gray-200">
+        <figure className="px-10 pt-10">
+            <div className="w-24 mx-auto">
+                <img
+                    src={thumbnail}
+                    alt="App"
+                    className="rounded-xl w-full"
+                />
+            </div>
+        </figure>
+        <div className="card-body items-center text-center">
+          <h2 className="card-title font-extrabold text-xl">{name}</h2>
+
+            <div className="space-y-4">
+                <p className="flex gap-2 justify-center text-base font-semibold items-center text-[#fa8b16]">{<FaStar size={20} color="#fa8b16"></FaStar>} {rating}</p>
+
+                <p className="flex gap-2 justify-center text-base font-semibold items-center text-accent"> { <FaDownload size={20}></FaDownload> } {downloads}</p>
+            </div>
+          
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default TrendingAppsCard;
